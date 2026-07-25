80c89ca93fba6fdb42f089537fe67b7e666a0a95
Mauricio Perdomo <32530656+maoper11@users.noreply.github.com>
2026-07-25 00:35:53 -0500

    chore: update ComfyUI clone URL

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = async (kernel, info) => {
       method: "shell.run",
       params: {
         message:
-          "git clone --branch {{ (() => { let ver = String(env.COMFY_VER || 'auto').trim().toLowerCase(); if (['', 'auto', 'latest'].includes(ver)) return 'master'; if (/^v/.test(ver)) return ver; if (/^\\d/.test(ver)) return 'v' + ver; return ver; })() }} --depth 1 https://github.com/comfyanonymous/ComfyUI app",
+          "git clone --branch {{ (() => { let ver = String(env.COMFY_VER || 'auto').trim().toLowerCase(); if (['', 'auto', 'latest'].includes(ver)) return 'master'; if (/^v/.test(ver)) return ver; if (/^\\d/.test(ver)) return 'v' + ver; return ver; })() }} --depth 1 https://github.com/Comfy-Org/ComfyUI.git app",
       },
     },
 
